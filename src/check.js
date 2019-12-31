e6e71256b2800ba03b62fd9038d562c3ad9bdcc8
sangunPark <sangunp703@naver.com>
2020-01-01 00:56:17 +0900

    회원가입 화면 구성

--- src/check.js
+++ src/check.js
@@ -1,32 +1,41 @@
 import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
 import '../style/css/check.css'
+import Join from './component/join'
 
 export default class Check extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      jump: ''
+      jump: '',
+      joinShow: false,
+      loginShow: false
     }
   }
+
+  openJoinBox(e) {
+    this.setState({ joinShow: true })
+  }
+
   render() {
     if (this.state.jump) {
       return <Redirect to={this.state.jump} />
     }
     return (
       <div className='check-container'>
-        <div className='part'>
-          <div className='login-box'></div>
-        </div>
-        <div className='part entrance'>
-          <div class="talk-box">
-            회원증이나 신분증을 보여주세요
+        <div className='partition'>
+          <div className='part'>
+            <div className='login-box'></div>
           </div>
-          <img className='security' src='' alt='silhouette' />
+          <div className='part entrance'>
+            <div class='talk-box'>회원증이나 신분증을 보여주세요</div>
+            <img className='security' src='' alt='silhouette' />
+          </div>
+          <div className='part'>
+            <img className='join-box' src='' alt='주민등록증' onClick={e => this.openJoinBox(e)}></img>
+          </div>
         </div>
-        <div className='part'>
-          <div className='join-box'></div>
-        </div>
+        <Join show={this.state.joinShow} />
       </div>
     )
   }
--- src/component/join.js
+++ src/component/join.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
-import '../style/css/join.css'
+import '../../style/css/join.css'
 
 export default class Join extends Component {
   constructor(props) {
@@ -9,13 +9,48 @@ export default class Join extends Component {
       jump: ''
     }
   }
+
+  componentDidUpdate() {
+    if (this.props.show) {
+      document.querySelector('.join-container').style.display = 'block'
+    }
+  }
+
+  subscription(e) {
+    const applyStyle = document.querySelector('.apply-after').style
+
+    applyStyle.opacity = '1'
+    applyStyle.bottom = '0%'
+    setTimeout(() => {
+      this.setState({ jump: '/intro' })
+    }, 1500)
+  }
+
+  closeBox(e) {
+    e.currentTarget.parentNode.style.display = 'none'
+  }
+
   render() {
     if (this.state.jump) {
       return <Redirect to={this.state.jump} />
     }
     return (
       <div className='join-container'>
-        <img className="" src='' alt='identity-card'/>
+        <div className='layout' onClick={e => this.closeBox(e)}></div>
+        <div className='content'>
+          <form className='info'>
+            <h1>주민등록증</h1>
+            <input type='text' />
+            <span>(한자)</span>
+            <input type='text' />
+            <span>-</span>
+            <input type='password' />
+            <input type='email' />
+          </form>
+          <img className='profile' />
+          <img className='apply' onClick={e => this.subscription(e)} />
+          <img className='apply-after' />
+        </div>
       </div>
     )
   }
