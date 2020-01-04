abb2ce1d3aa8e6759df92224440d28bfbd312fc8
sangunPark <sangunp703@naver.com>
2020-01-05 02:38:42 +0900

    main 세부 수정, menu 컴포넌트 생성

---
+++ src/component/menu.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import '../../style/css/menu.css'
+
+export default class Menu extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidUpdate() {
+    if (this.props.show === 'menu') {
+      document.querySelector('.menu-container').style.display = 'block'
+    }
+  }
+
+  closeBox(e) {
+    this.props.showCallback('none')
+    e.currentTarget.parentNode.style.display = 'none'
+  }
+
+  render() {
+    return (
+      <div className='menu-container'>
+        <div className='layout' onClick={e => this.closeBox(e)}></div>
+        <div className='background'>
+          <div className='menu'>
+            <h1>M E N U</h1>
+            <div className='content'>
+              <div className='alcohol'>
+                <img className='bottle' src='' alt='bottle' />
+                <img className='glass' src='' alt='glass' />
+                <span className='reply-count'>X 1</span>
+              </div>
+              <div className='worry'>
+                <h2 className='title'>it's sad</h2>
+                <pre className='story'>blah blah</pre>
+              </div>
+            </div>
+            <img className='arrow-back' src='' alt='previous' />
+            <img className='arrow-next' src='' alt='next' />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
 import '../style/css/main.css'
+import Menu from './component/menu'
 
-export default class Check extends Component {
+export default class Main extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      jump: ''
+      jump: '',
+      show: 'none'
     }
+    this.showCallback = this.showCallback.bind(this)
+  }
+
+  showCallback(showData) {
+    this.setState({
+      show: showData
+    })
+  }
+
+  openMenuBox(e) {
+    this.setState({ show: 'menu' })
   }
 
   render() {
@@ -18,7 +31,12 @@ export default class Check extends Component {
       <div className='main-container'>
         <div className='layout-left'></div>
         <div className='layout-right'></div>
-        <div className='table'></div>
+        <img className='bartender' src='' alt='bartender' />
+        <img className='table' src='' alt='table'></img>
+        <div className='menu' onClick={e => this.openMenuBox(e)}>
+          MENU
+        </div>
+        <Menu show={this.state.show} showCallback={this.showCallback} />
       </div>
     )
   }
